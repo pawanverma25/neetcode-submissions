ab7c0baa9f4845f7f95385c681ea36cbfc5bd232
Pawan Verma <pawanzofficial@gmail.com>
2026-09-03 16:08:34 +0530

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        List<Integer> res = new ArrayList<>();
+        Deque<Integer> dq = new LinkedList<>();
+        for(int i = 0; i < nums.length; i++){
+            if(!dq.isEmpty() && dq.peekFirst() == i - k) dq.pollFirst();
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]){
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+            if(i >= k - 1) res.add(nums[dq.peekFirst()]);
+        }
+        return res.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
